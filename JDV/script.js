690e948d8efb26a811c84795997528dd781014df
root <root@Augusto.localdomain>
2022-01-12 10:54:06 -0300

    atualizando as variaveis para dentro da classe no exercicio do jodo da velha

--- JDV/script.js
+++ JDV/script.js
@@ -1,100 +1,87 @@
 class JogoDaVelha {
+    constructor(){
+        this.aux = 0
+        this.jogoAtual = [   //casas ocupadas
+            ['', '', ''],
+            ['', '', ''],
+            ['', '', '']
+        ]
+    }
     ColocaNaTabela(movimento){
         let click = document.getElementById(movimento)
         if(movimento[0] == 'a'){
             if(movimento[1] == '1'){
-                if(jogoAtual[0][0] == ''){
-                    movimento[2] == 'x' ? jogoAtual[0][0] = 'x' : jogoAtual[0][0] = 'o'
+                if(this.jogoAtual[0][0] == ''){
+                    movimento[2] == 'x' ? this.jogoAtual[0][0] = 'x' : this.jogoAtual[0][0] = 'o'
                     click.style.display = 'block'
-                } else {aux = !aux; JDV.InvalidOperationException()}
+                } else {this.aux = !this.aux; JDV.InvalidOperationException()}
             }
             if(movimento[1] == '2'){
-                if(jogoAtual[0][1] == ''){
-                    movimento[2] == 'x' ? jogoAtual[0][1] = 'x' : jogoAtual[0][1] = 'o'
+                if(this.jogoAtual[0][1] == ''){
+                    movimento[2] == 'x' ? this.jogoAtual[0][1] = 'x' : this.jogoAtual[0][1] = 'o'
                     click.style.display = 'block'
-                } else {aux = !aux; JDV.InvalidOperationException()}
+                } else {this.aux = !this.aux; JDV.InvalidOperationException()}
             }
             if(movimento[1] == '3'){
-                if(jogoAtual[0][2] == ''){
-                    movimento[2] == 'x' ? jogoAtual[0][2] = 'x' : jogoAtual[0][2] = 'o'
+                if(this.jogoAtual[0][2] == ''){
+                    movimento[2] == 'x' ? this.jogoAtual[0][2] = 'x' : this.jogoAtual[0][2] = 'o'
                     click.style.display = 'block'
-                } else {aux = !aux; JDV.InvalidOperationException()}
+                } else {this.aux = !this.aux; JDV.InvalidOperationException()}
             }
         }
         if(movimento[0] == 'b'){
             if(movimento[1] == '1'){
-                if(jogoAtual[1][0] == ''){
-                    movimento[2] == 'x' ? jogoAtual[1][0] = 'x' : jogoAtual[1][0] = 'o'
+                if(this.jogoAtual[1][0] == ''){
+                    movimento[2] == 'x' ? this.jogoAtual[1][0] = 'x' : this.jogoAtual[1][0] = 'o'
                     click.style.display = 'block'
-                } else {aux = !aux; JDV.InvalidOperationException()}
+                } else {this.aux = !this.aux; JDV.InvalidOperationException()}
             }
             if(movimento[1] == '2'){
-                if(jogoAtual[1][1] == ''){
-                    movimento[2] == 'x' ? jogoAtual[1][1] = 'x' : jogoAtual[1][1] = 'o'
+                if(this.jogoAtual[1][1] == ''){
+                    movimento[2] == 'x' ? this.jogoAtual[1][1] = 'x' : this.jogoAtual[1][1] = 'o'
                     click.style.display = 'block'
-                } else {aux = !aux; JDV.InvalidOperationException()}
+                } else {this.aux = !this.aux; JDV.InvalidOperationException()}
             }
             if(movimento[1] == '3'){
-                if(jogoAtual[1][2] == ''){
-                    movimento[2] == 'x' ? jogoAtual[1][2] = 'x' : jogoAtual[1][2] = 'o'
+                if(this.jogoAtual[1][2] == ''){
+                    movimento[2] == 'x' ? this.jogoAtual[1][2] = 'x' : this.jogoAtual[1][2] = 'o'
                     click.style.display = 'block'
-                } else {aux = !aux; JDV.InvalidOperationException()}
+                } else {this.aux = !this.aux; JDV.InvalidOperationException()}
             }
         }
         if(movimento[0] == 'c'){
             if(movimento[1] == '1'){
-                if(jogoAtual[2][0] == ''){
-                    movimento[2] == 'x' ? jogoAtual[2][0] = 'x' : jogoAtual[2][0] = 'o'
+                if(this.jogoAtual[2][0] == ''){
+                    movimento[2] == 'x' ? this.jogoAtual[2][0] = 'x' : this.jogoAtual[2][0] = 'o'
                     click.style.display = 'block'
-                } else {aux = !aux; JDV.InvalidOperationException()}
+                } else {this.aux = !this.aux; JDV.InvalidOperationException()}
             }
             if(movimento[1] == '2'){
-                if(jogoAtual[2][1] == ''){
-                    movimento[2] == 'x' ? jogoAtual[2][1] = 'x' : jogoAtual[2][1] = 'o'
+                if(this.jogoAtual[2][1] == ''){
+                    movimento[2] == 'x' ? this.jogoAtual[2][1] = 'x' : this.jogoAtual[2][1] = 'o'
                     click.style.display = 'block'
-                } else {aux = !aux; JDV.InvalidOperationException()}
+                } else {this.aux = !this.aux; JDV.InvalidOperationException()}
             }
             if(movimento[1] == '3'){
-                if(jogoAtual[2][2] == ''){
-                    movimento[2] == 'x' ? jogoAtual[2][2] = 'x' : jogoAtual[2][2] = 'o'
+                if(this.jogoAtual[2][2] == ''){
+                    movimento[2] == 'x' ? this.jogoAtual[2][2] = 'x' : this.jogoAtual[2][2] = 'o'
                     click.style.display = 'block'
-                } else {aux = !aux; JDV.InvalidOperationException()}
+                } else {this.aux = !this.aux; JDV.InvalidOperationException()}
             }
         }
-        JDV.VerificaVencedor()
+        JDV.ObterVencedor()
     }
     Jogar(x, y) {
-        if (aux == 0) {
+        let atualJogador = ''   //alterna para x ou o
+        if (this.aux == 0) {
             atualJogador = 'x'
-            aux = 1
+            this.aux = 1
         } else {
             atualJogador = 'o'
-            aux = 0
+            this.aux = 0
         }
         let movimento = x + y + atualJogador
         JDV.ColocaNaTabela(movimento)
-        console.log(jogoAtual)
-    }
-    
-    VerificaVencedor() { //logica para verificar se há vencedor
-        for(let a = 0; a < jogoAtual.length; a++){
-            if(jogoAtual[0][a] == jogoAtual[1][a] && jogoAtual[1][a] == jogoAtual[2][a] && jogoAtual[2][a] != ''){  //verifica as linhas
-                alert(`Vencedor: ${jogoAtual[0][a]}`)
-                vencedor = jogoAtual[0][a]
-            }
-            if(jogoAtual[a][0] == jogoAtual[a][1] && jogoAtual[a][1] == jogoAtual[a][2] && jogoAtual[a][2] != ''){  //verifica as colunas
-                alert(`Vencedor: ${jogoAtual[a][0]}`)
-                vencedor = jogoAtual[a][0]
-            }
-        }
-        if(jogoAtual[0][0] == jogoAtual [1][1] && jogoAtual [1][1] == jogoAtual[2][2] && jogoAtual[2][2] != ''){
-            alert(`Vencedor: ${jogoAtual[0][0]}`)
-            vencedor = jogoAtual[0][0]
-        }
-        if(jogoAtual[0][2] == jogoAtual [1][1] && jogoAtual[1][1] == jogoAtual[2][0] && jogoAtual[2][0] != ''){
-            alert(`Vencedor: ${jogoAtual[0][2]}`)
-            vencedor = jogoAtual[0][2]
-        }
     }
     NovoJogo(){
         location.reload()
@@ -106,16 +93,27 @@ class JogoDaVelha {
         alert('Posição fora do tabuleiro')
     }
     ObterVencedor() {    //string que retorna 'o', 'x', ou 'null'
+        let vencedor = ''   //recebe o vencedor
+        for(let a = 0; a < this.jogoAtual.length; a++){
+            if(this.jogoAtual[0][a] == this.jogoAtual[1][a] && this.jogoAtual[1][a] == this.jogoAtual[2][a] && this.jogoAtual[2][a] != ''){  //verifica as linhas
+                alert(`Vencedor: ${this.jogoAtual[0][a]}`)
+                vencedor = this.jogoAtual[0][a]
+            }
+            if(this.jogoAtual[a][0] == this.jogoAtual[a][1] && this.jogoAtual[a][1] == this.jogoAtual[a][2] && this.jogoAtual[a][2] != ''){  //verifica as colunas
+                alert(`Vencedor: ${this.jogoAtual[a][0]}`)
+                vencedor = this.jogoAtual[a][0]
+            }
+        }
+        if(this.jogoAtual[0][0] == this.jogoAtual [1][1] && this.jogoAtual [1][1] == this.jogoAtual[2][2] && this.jogoAtual[2][2] != ''){
+            alert(`Vencedor: ${this.jogoAtual[0][0]}`)
+            vencedor = this.jogoAtual[0][0]
+        }
+        if(this.jogoAtual[0][2] == this.jogoAtual [1][1] && this.jogoAtual[1][1] == this.jogoAtual[2][0] && this.jogoAtual[2][0] != ''){
+            alert(`Vencedor: ${this.jogoAtual[0][2]}`)
+            vencedor = this.jogoAtual[0][2]
+        }
         return vencedor
     }
 }
 
-let jogoAtual = [   //casas ocupadas
-    ['', '', ''],
-    ['', '', ''],
-    ['', '', '']
-]
-let vencedor = ''   //recebe o vencedor
-let atualJogador = ''   //alterna para x ou o
-let aux = 0     //alterna para x ou o
 let JDV = new JogoDaVelha()
