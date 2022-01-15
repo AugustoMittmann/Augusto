4fbde7884324641376f0cb6c505b5fd2f2c74e76
Augusto Mittmann <gutomittmann10@gmail.com>
2022-01-15 11:43:03 -0300

    corrigindo lógica da validacao de string usando pilhas

--- validacao.js
+++ validacao.js
@@ -1,25 +1,62 @@
 function string(originalString) {
-    let chave = 0, colchete = 0, parenteses = 0
-
+    let chave = 0
+    let colchete = 0
+    let parenteses = 0
+    let pilha = []
+  
     for(let a = 0; a < originalString.length; a++) {
-        if(!isNaN(originalString[a]) && originalString[a] != ' '){
-            return `String inválida`
-        } else {
-            if(originalString[a] == `{`){chave++}
-            if(originalString[a] == `}`){chave--}
-            if(originalString[a] == `(`){parenteses++}
-            if(originalString[a] == `)`){parenteses--}
-            if(originalString[a] == `[`){colchete++}
-            if(originalString[a] == `]`){colchete--}
-            if(chave < 0 || parenteses < 0 || colchete < 0 ){ return `String inválida` }
-        }
-    }
+  
+      if(!isNaN(originalString[a]) && originalString[a] != ' ') {
+          return `String inválida`
+      } else {
+      if(originalString[a] == `{`) {
+          chave++
+          pilha.push('{')
+      }
+      if(originalString[a] == `}`) {
+          chave--
+          if(pilha[pilha.length-1] == '{'){
+              pilha.pop()
+          } else {
+              return 'String Inválida'
+          }
+      }
+      if(originalString[a] == `(`) {
+          pilha.push('(')
+          parenteses++
+      }
+      if(originalString[a] == `)`) {
+          parenteses--
+          if(pilha[pilha.length-1] == '('){
+              pilha.pop()
+          } else {
+              return 'String Inválida'
+          }
+      }
+      if(originalString[a] == `[`) {
+          pilha.push('[')
+          colchete++
+      }
+      if(originalString[a] == `]`) {
+          colchete--
+          if(pilha[pilha.length-1] == '['){
+              pilha.pop()
+          } else {
+              return 'String Inválida'
+          }
+      }
+      if(chave < 0 || parenteses < 0 || colchete < 0 ){ return `String inválida` }
+      }
+  }
+  pilha = []
+  
     if(chave == 0 && colchete == 0 && parenteses == 0){return `String válida`} else{return `String inválida`}
-}
-
-
-console.log(string(`[ (abc) ]`))
-console.log(string(`[{(a)}bc{}()]`))
-console.log(string(`[(abc]`))
-console.log(string(`[{)x}]`))
-console.log(string(`[ (1) ]`))
+  }
+  
+  
+  console.log(string(`[ (abc) ]`))
+  console.log(string(`[{(a)}bc{}()]`))
+  console.log(string(`[(abc]`))
+  console.log(string(`[{)x}]`))
+  console.log(string(`[ (1) ]`))
+  console.log(string('([)]'))
