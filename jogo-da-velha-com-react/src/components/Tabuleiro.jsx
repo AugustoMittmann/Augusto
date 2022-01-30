bbfd78b7351bb67bac845e09876d6d4212bb0b39
Augusto Mittmann Note <gutomittmann10@gmail.com>
2022-01-30 20:56:31 -0300

    adicionais presencial

--- jogo-da-velha-com-react/src/components/Tabuleiro.jsx
+++ jogo-da-velha-com-react/src/components/Tabuleiro.jsx
@@ -2,6 +2,8 @@ import React, { useState } from "react"
 import './Style.css'
 import Icone from "./Icone"
 
+let historicoDeJogos = []
+let qntdVencedores = [0, 0]
 
 const Tabuleiro = () => {
   const estadoInicial = [
@@ -14,16 +16,31 @@ const Tabuleiro = () => {
   
   function verificaVencedor(e) {
     for(let a = 0; a < 3; a++) {
-      if(e[0][a] === e[1][a] && e[1][a] === e[2][a] && e[2][a] !== '') return e[0][a]  //coluna
-      if(e[a][0] === e[a][1] && e[a][1] === e[a][2] && e[a][2] !== '') return e[a][0] //linha
+      if(e[0][a] === e[1][a] && e[1][a] === e[2][a] && e[2][a] !== '') {    //coluna
+        return e[0][a]
+      }
+      if(e[a][0] === e[a][1] && e[a][1] === e[a][2] && e[a][2] !== '') {  //linha
+        return e[a][0]
+      }
     }
-    if(e[0][0] === e[1][1] && e[1][1] === e[2][2] && e[0][0] !== '') return e[0][0]
-    if(e[0][2] === e[1][1] && e[1][1] === e[2][0] && e[1][1] !== '') return e[0][2]
+    if(e[0][0] === e[1][1] && e[1][1] === e[2][2] && e[0][0] !== '') { 
+      return e[0][0]
+    }
+    if(e[0][2] === e[1][1] && e[1][1] === e[2][0] && e[1][1] !== '') {
+      return e[0][2]
+    }
   }
-
+  
   function NovoJogo() {
+
     setSituacaoDoJogo(estadoInicial)
     setCurrentPlayer('x')
+    historicoDeJogos.push({situacaoDoJogo, winner})
+    console.table(historicoDeJogos)
+  }
+
+  function callBackGame() {
+    setSituacaoDoJogo(historicoDeJogos[historicoDeJogos.length-1].situacaoDoJogo)
   }
   
   function click(x, y) {
@@ -38,13 +55,15 @@ const Tabuleiro = () => {
       }))
       setCurrentPlayer(currentPlayer === 'x' ? 'o' : 'x')
     } else {
-      console.log('This position already was click');
+      console.log('This position already was click')
     }
+    console.table(historicoDeJogos)
   }
   const winner = verificaVencedor(situacaoDoJogo)
 
   return (
-    <div className="tabuleiro">
+    <>
+      <div className="tabuleiro">
       {
         situacaoDoJogo.map((linha, i) => {
           return linha.map((celula, j) => {
@@ -54,11 +73,20 @@ const Tabuleiro = () => {
           })
         })
       }
+      </div>
       <input className='newGame' type="button" value="Novo jogo" onClick={NovoJogo}/>
+      <input className='newGame' type="button" value="Mostrar jogo anterior" onClick={callBackGame}/>
       {
-        winner ? <h1>Vencedor: {winner} </h1> : null 
+        winner ? <h1>Vencedor: {winner} </h1> : null
       }
-    </div>
+
+      <input list='oldGames' />
+      <datalist id='oldGames'>
+        <option value='jogo 1'/>
+        <option value='jogo 2'/>
+        <option value='jogo 3'/>
+      </datalist>
+    </>
   )
 }
 
